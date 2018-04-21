08fb2cd9330be23f25035de6082a23d70b05c7cd
guille31794 <guille.g94@hotmail.es>
2018-04-21 13:42:33 +0200

    Añadidas dos clases mas, Clave y Numero

---
+++ P2/clave.hpp
@@ -0,0 +1,17 @@
+/*
+  Creado por Guillermo Girón García para la asignatura
+  POO de la UCA.
+
+  21/04/2018, GPL Licensed, all rights reserved */
+
+  #include "cadena.hpp"
+
+  class Clave
+  {
+    private:
+
+
+    public:
+      Cadena clave() const;
+      bool verifica(const char *);
+  };
---
+++ P2/numero.hpp
@@ -0,0 +1,13 @@
+/*
+  Creado por Guillermo Girón García para la asignatura
+  POO de la UCA.
+
+  21/04/2018, GPL Licensed, all rights reserved */
+
+  class Numero {
+  private:
+
+
+  public:
+    operator const char*() const;
+  };
