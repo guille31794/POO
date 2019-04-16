102771b8b8c9174cd8301212706da41cc1514daf
Guillermo Girón <guillermogiron@pop-os.localdomain>
2019-04-16 14:39:32 +0200

    usuario.hpp done!

---
+++ P2/usuario.hpp
@@ -0,0 +1,64 @@
+#ifndef Usuario_HPP_
+#define Usuario_HPP_
+
+#include "cadena.hpp"
+#include "clave.hpp"
+#include "tarjeta.hpp"
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+class Usuario
+{
+    public:
+        //Asociative  data types
+        typedef map<Numero, Tarjeta*> Tarjetas;
+        typedef unordered_map<Articulo*, unsigned> Articulos;
+    
+        //Constructor
+        Usuario(const char*, const char*, const char*, const char*,
+        const char*);
+        Usuario(const Usuario&) = delete;
+        Usuario& operator =(const Usuario&) = delete;
+
+        //Destructor
+        ~Usuario();
+
+        //Getters
+        Cadena id() const;
+        Cadena nombre() const;
+        Cadena apellidos() const;
+        Cadena direccion() const;
+        Tarjetas tarjetas() const;
+        unsigned n_articulos() const;
+        Articulos compra() const;
+
+        //Setters
+
+        //Asociative data methods
+        void es_titular_de(const Tarjeta&);
+        void no_es_titular_de(const Tarjeta&);
+        void compra(const Articulo&, const unsigned = 1);
+
+        //Exception stuff
+        class Id_duplicado
+        {
+            public:
+                Id_duplicado(const Cadena&);
+                Cadena idd() const;
+            private:
+                Cadena id;
+        };
+    private:
+        Cadena identifier, name, surname, address;
+        Clave password;
+        Tarjetas cards;
+        Articulos shopping_kart;
+};
+
+//Operator
+ostream& operator <<(ostream& os, const Usuario& u);
+ostream& mostrar_carro(ostream&, const Usuario::Articulos&);
+
+#endif
